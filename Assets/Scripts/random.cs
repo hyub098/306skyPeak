e2f49b323861701378481c1b2a1cbf19ed0e13e7
朴思扬 <LeonPark@wifi-172-23-11-42.uoa-wifi.auckland.ac.nz>
2015-10-03 12:28:03 +1300

    Random generation

---
+++ Assets/Scripts/random.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class random : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		transform.position=new Vector3(150+50*Random.value, 150,150+50*Random.value );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
