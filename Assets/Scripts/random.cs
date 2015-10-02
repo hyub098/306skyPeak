2609e77d895897e017fe08c646cad928c8547708
faridaelz <felz123@aucklanduni.ac.nz>
2015-10-03 12:56:37 +1300

    initial commit

--- Assets/Scripts/random.cs
+++ Assets/Scripts/random.cs
@@ -5,11 +5,13 @@ public class random : MonoBehaviour {
 	
 	// Use this for initialization
 	void Start () {
+
 		transform.position=new Vector3(150+50*Random.value, 150,150+50*Random.value );
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        // hi there
 		
 	}
 }
