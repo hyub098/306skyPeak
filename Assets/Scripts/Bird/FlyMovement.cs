368ff728263b8d021ce0ebb800e4dd71c8b2d8d1
hyub098 <hyub098@aucklanduni.ac.nz>
2015-10-03 18:12:17 +1300

    fly movement

--- Assets/Scripts/Bird/FlyMovement.cs
+++ Assets/Scripts/Bird/FlyMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 public class FlyMovement : MonoBehaviour {
 	
 	public float speed;
+	private float inputSpd;
 //	public float moveSpd;
 	
 //	public Text winText;
@@ -15,11 +16,12 @@ public class FlyMovement : MonoBehaviour {
 	private float rotationX;
 	private float rotationZ;
 	private Vector3 movement;
+	private float v;
 	private Rigidbody rb;
 
 	private Animation anim;
-	private float camRayLength = 100;
-	private int floorMask;
+	private Vector3 moveDistance;
+
 
 
 //    int incrementTime = 1;
@@ -37,10 +39,13 @@ public class FlyMovement : MonoBehaviour {
     // Use this for initialization
     void Start () {
 		Debug.Log ("plane pilot script added to: " + gameObject.name);
+		inputSpd = 0;
+		moveDistance = new Vector3(0,0,0);
 
 		anim = GetComponent<Animation> ();
 		rb = GetComponent<Rigidbody> ();
 
+		float v = 0;
      //   SetTimerText();
 
         //		SetCountText();
@@ -49,7 +54,7 @@ public class FlyMovement : MonoBehaviour {
     // Update is called once per frame
     void Update () {
 
-
+		inputSpd = Mathf.Clamp (inputSpd, 1, 20);
 //        minute = (int)counter / 60;
 //        second = (int)counter % 60;
 //        time += Time.deltaTime;
@@ -70,33 +75,36 @@ public class FlyMovement : MonoBehaviour {
 
 
 
-		float h = Input.GetAxisRaw("Horizontal");
-		float v = Input.GetAxisRaw("Vertical");
-		Move (h, v);
-	//	Turning ();
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis ("Vertical");
+//		if (Input.GetKeyDown ("up")) {
+//			v = 1;
+//		} else if (Input.GetKeyDown ("down")) {
+//			v = -1;
+//		}else{ v=0;}
 
-//		if (v != 0) {
-//			anim.Play("flyNormal");
-//			speed = Mathf.Clamp (speed, 1, 20);
-//
-//			speed = v * speed;
-//		}
-//
-//
-//		//move the plane
-//		transform.position += transform.forward * Time.deltaTime * speed;
-//	
-//	//	if (Rotation ()) {
-//			//rotate the plane from input
-//			transform.Rotate (-Input.GetAxis("Vertical"),0.0f, -Input.GetAxis("Horizontal"));
-//	//	}
+		if (transform.position.y != 0) {
+			inputSpd = speed;
+			anim.Play ("flyNormal");
+		}
+		//	inputSpd = v * inputSpd;
+		if (Input.GetKey ("space")) {
 
-//		//move the plane
-//		transform.position += transform.forward * Time.deltaTime * speed;
-//	
+			moveDistance = transform.forward * Time.deltaTime * inputSpd;
+			transform.position += moveDistance;
+
+		} else {
+			if(moveDistance.magnitude == 0){ moveDistance = Vector3.zero;}
+
+			transform.position += 10*moveDistance/inputSpd;
+			 
+			moveDistance = moveDistance - moveDistance/10;
+		}
+		//move the plane
+
 //		if (Rotation ()) {
-//			//rotate the plane from input
-//			transform.Rotate (-Input.GetAxis("Vertical")*2,0.0f, -Input.GetAxis("Horizontal")*3);
+			//rotate the plane from input
+			transform.Rotate (/*-Input.GetAxis("Vertical")*1.5f*/ -v,0.0f, -h*2);
 //		}
 
 
@@ -121,7 +129,7 @@ public class FlyMovement : MonoBehaviour {
 			rotate = false;
 
 			if(transform.eulerAngles.x < 100){
-				transform.eulerAngles = new Vector3(45,transform.eulerAngles.y, transform.eulerAngles.z);
+				transform.eulerAngles = new Vector3(44.9f,transform.eulerAngles.y, transform.eulerAngles.z);
 			}
 			else{
 				transform.eulerAngles = new Vector3(315,transform.eulerAngles.y, transform.eulerAngles.z);
@@ -154,33 +162,5 @@ public class FlyMovement : MonoBehaviour {
 
 
 
-	void Move(float h, float v)
-	{
-		movement.Set (v/10, v, h);
 
-		if (h != 0 || v != 0) {
-			anim.Play("flyNormal");
-		}
-		
-		movement = movement.normalized * speed * Time.deltaTime;
-		
-		rb.MovePosition (transform.position + movement);
-		transform.Rotate (-Input.GetAxis("Vertical"),0.0f, -Input.GetAxis("Horizontal"));
-	}
-
-
-	void Turning(){
-		
-		Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
-		
-		RaycastHit floorHit;
-		
-		if (Physics.Raycast (camRay, out floorHit, camRayLength, floorMask)) {
-			Vector3 playerToMouse = floorHit.point - transform.position;
-			playerToMouse.y = 0f;
-			
-			Quaternion newRotation = Quaternion .LookRotation(playerToMouse);
-			rb.MoveRotation(newRotation);
-		}
-	}
 }
