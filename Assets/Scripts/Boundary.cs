0e69da9dd397eb46b468781eaa18a7014d44c300
Davidjin0418 <bjin718@aucklanduni.ac.nz>
2015-10-04 14:32:34 +1300

    update boundary,change force to setting position

--- Assets/Scripts/Boundary.cs
+++ Assets/Scripts/Boundary.cs
@@ -5,12 +5,12 @@ public enum Direction
 	minX, maxX, minY, maxY,minZ,maxZ,normal
 }
 public class Boundary : MonoBehaviour {
-	int minX = -200; //left boundary 
-	int maxX = 200; //right boundary 
-	int minY = -200; // up boundary 
-	int maxY = 200; // down boundary
-	int maxZ =200;
-	int minZ =-200;
+	int minX = -600; //left boundary 
+	int maxX = 600; //right boundary 
+	int minY = -600; // up boundary 
+	int maxY = 600; // down boundary
+	int maxZ =600;
+	int minZ =-600;
 	public Rigidbody rb;
 	float strengh=1.0f;
 	bool ispushing=false;
@@ -23,41 +23,39 @@ public class Boundary : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		rb.AddForce (new Vector3 (0, 0,-10));
-		rb.AddForce (new Vector3 (0, 0,10));
 		outofbounds ();
 	}
 	
 	void outofbounds() { 
 		
 		if (rb.transform.position.x < minX) {
-			rb.AddForce (new Vector3 (10, 0, 0));
+			transform.position=new Vector3 (minX, transform.position.y, transform.position.z);
 			ispushing = true;
 		} else if (rb.transform.position.x > maxX) {
-			rb.AddForce (new Vector3 (-10, 0, 0));
+			transform.position=new Vector3 (maxX, transform.position.y, transform.position.z);
 			d = Direction.maxX;
 			ispushing = true;
 		} else if (rb.transform.position.z < minZ) {
-			rb.AddForce (new Vector3 (0, 0, 10));
+			transform.position=new Vector3 (transform.position.x, transform.position.y, minZ);
 			d = Direction.minZ;
 			ispushing = true;
 		} else if (rb.transform.position.z > maxZ) {
-			rb.AddForce (new Vector3 (0, 0, -10));
+			transform.position=new Vector3 (transform.position.x, transform.position.y,maxZ);
 			Debug.Log ("reach maxZ");
 			d = Direction.maxZ;
 			ispushing = true;
 		} else if (rb.transform.position.y < minY) {
-			rb.AddForce (new Vector3 (0, 10, 0));
+			transform.position=new Vector3 (transform.position.x, minY, transform.position.z);
 			d = Direction.minY;
 			ispushing = true;
 		} else if (rb.transform.position.y > maxY) { 
-			rb.AddForce (new Vector3 (0, -10, 0)); 
+			transform.position=new Vector3 (transform.position.x, maxY, transform.position.z);
 			d = Direction.maxY;
 			ispushing = true;
 		} else {
 			if(ispushing){
 				ispushing=false;
-				rb.velocity = Vector3.zero;
+				//rb.velocity = Vector3.zero;
 			}
 		}
 		
