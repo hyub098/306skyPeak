c1e2de2d829b95bdbd4067796fa3a37475ecc71a
hyub098 <hyub098@aucklanduni.ac.nz>
2015-10-05 00:51:12 +1300

    minimapProgress

--- Assets/Scripts/Camera/Radar.cs
+++ Assets/Scripts/Camera/Radar.cs
@@ -2,11 +2,21 @@
 using System.Collections;
 using System.Collections.Generic;
 
+
+/**
+ * 
+ * Reference to Youtuber:Florian M
+ */
+
 public class Radar : MonoBehaviour {
 
 	public GameObject[] trackerdObjs;
 	public GameObject radarPrefab;
 	List<GameObject> radarObjs; 
+	List<GameObject> borderObjs; 
+	public float switchDist;
+	public Transform helpTransform;
+	public GameObject player;
 
 
 	// Use this for initialization
@@ -14,20 +24,39 @@ public class Radar : MonoBehaviour {
 		createRadarObj ();
 
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+		transform.position = new Vector3 (player.transform.position.x, player.transform.position.y + 349f, player.transform.position.z);
+
+
+		for(int i =0; i<radarObjs.Count;i++){
+			if(Vector3.Distance(radarObjs[i].transform.position,transform.position) > switchDist){
+				helpTransform.LookAt(radarObjs[i].transform);
+				borderObjs[i].transform.position = transform.position + switchDist * helpTransform.forward;
+				borderObjs[i].layer = LayerMask.NameToLayer("Radar");
+				radarObjs[i].layer = LayerMask.NameToLayer("Invisible");
+			}
+			else{
+				radarObjs[i].layer = LayerMask.NameToLayer("Radar");
+				borderObjs[i].layer = LayerMask.NameToLayer("Invisible");
+			}
+			 
+		}
 	}
 
 
 	void createRadarObj(){
 		radarObjs = new List<GameObject> ();
-		Debug.Log("Script is loaded: " + gameObject.name);
+		borderObjs = new List<GameObject>();
+
 		foreach (GameObject o in trackerdObjs) {
 			GameObject k = Instantiate(radarPrefab,o.transform.position,Quaternion.identity) as GameObject;
-
 			radarObjs.Add(k);
+
+
+			GameObject j = Instantiate(radarPrefab,o.transform.position,Quaternion.identity) as GameObject;
+			borderObjs.Add(j);
 		}
 	}
 }
