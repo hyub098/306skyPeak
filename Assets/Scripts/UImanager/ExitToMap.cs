4cb1138c9fe5a28e0be3e4d9a6c5a795dace49ac
hyub098 <hyub098@aucklanduni.ac.nz>
2015-10-05 18:18:30 +1300

    demo fixed

---
+++ Assets/Scripts/UImanager/ExitToMap.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitToMap : MonoBehaviour {
+
+	public void OnClick(){
+		Application.LoadLevel ("map");
+	}
+}
---
+++ Assets/Scripts/UImanager/Resume.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Resume : MonoBehaviour {
+	public Canvas canvas;
+	
+
+
+	public void OnClick(){
+		Time.timeScale = 1f; //Resume game
+		canvas.enabled = false;
+	}
+}
