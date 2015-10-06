17b9f24965434f6193bc71176b83a1b85e0c37cf
Ivy Wang <swy0930@163.com>
2015-10-06 16:54:37 +1300

    add level 2 music

---
+++ Assets/Scripts/Music/ChangeMusic.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeMusic : MonoBehaviour {
+
+    public AudioClip level2Music;
+    public AudioClip level1Music;
+
+    private AudioSource source;
+
+    private int count = 0;
+
+
+
+    // Use this for initialization
+    void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    //void Update()
+    //{
+      //  if (_level != 2 && source!=null)
+       // {
+         //   source.Stop();
+        //}
+    //}
+
+    void OnLevelWasLoaded(int level)
+    {
+        if (level == 2)
+        {
+            count = count + 1;
+            source.clip = level2Music;
+            source.Play();
+        }
+        else if(count != 0)
+        {
+            source.clip = level1Music;
+            source.Play();
+        }
+
+    }
+}
