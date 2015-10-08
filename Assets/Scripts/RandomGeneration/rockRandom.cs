1989cb4f4c27b900ecf049c720f81148635c199e
Leonsy <leonsy5222@gmail.com>
2015-10-08 18:20:35 +1300

    Rock random generation

--- Assets/Scripts/RandomGeneration/rockRandom.cs
+++ Assets/Scripts/RandomGeneration/rockRandom.cs
@@ -3,14 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using System;
 //This calss is for random generation
-public class NewRandom : MonoBehaviour {
+public class rockRandom : MonoBehaviour {
 	public List<Vector3> myList = new List<Vector3>();
-
+	
 	// Use this for initialization
 	void Start () {
 		// Generate the object in a certain area
 		//transform.position=new Vector3(100+500*Random.value, 400,800*Random.value );
-
+		
 		myList.Add(new Vector3(27,4, 67 ));
 		myList.Add(new Vector3(50,4, 67 ));
 		myList.Add(new Vector3(78,4, 67 ));
@@ -21,12 +21,12 @@ public class NewRandom : MonoBehaviour {
 		myList.Add(new Vector3(89,4, 67 ));
 		myList.Add(new Vector3(9,4, 67 ));
 		myList.Add(new Vector3(57+UnityEngine.Random.value*3,4, 67 ));
-
+		
 		int number = Mathf.RoundToInt(UnityEngine.Random.value*10);
 		transform.position=myList[number];
-
-
-
+		
+		
+		
 	}
 	
 	// Update is called once per frame
