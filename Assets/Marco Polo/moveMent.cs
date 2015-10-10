e39ceb5419c28157071e27de3684426796a0ff7a
Leonsy <leonsy5222@gmail.com>
2015-10-10 14:23:38 +1300

    New way to do online multi

---
+++ Assets/Marco Polo/moveMent.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveMent : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		InputMovement ();
+	}
+
+	private void InputMovement()
+	{
+		if (Input.GetKey(KeyCode.W))
+			GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + Vector3.forward * 10 * Time.deltaTime);
+		
+		if (Input.GetKey(KeyCode.S))
+			GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position - Vector3.forward * 10 * Time.deltaTime);
+		
+		if (Input.GetKey(KeyCode.D))
+			GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + Vector3.right * 10 * Time.deltaTime);
+		
+		if (Input.GetKey(KeyCode.A))
+			GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position - Vector3.right * 10 * Time.deltaTime);
+	}
+}
