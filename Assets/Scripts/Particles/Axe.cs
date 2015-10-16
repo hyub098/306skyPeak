97eced8f8c58f08ceaac4906bcd61285f81b1bb9
Ivy Wang <swy0930@163.com>
2015-10-17 11:17:58 +1300

    add behaviour to axe

---
+++ Assets/Scripts/Particles/Axe.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Axe : MonoBehaviour {
+    public GameObject owl;
+    public GameObject axe;
+    private float speed = 20;
+    private Vector3 location;
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        location = owl.transform.position;
+        axe.transform.Rotate(-90f, 0.0f, 0.0f);
+        axe.transform.TransformDirection(location);
+    }
+}
