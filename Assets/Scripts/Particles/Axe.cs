e0aad8f70820cbe8e61ce8fc54f212c4c8895172
Ivy Wang <swy0930@163.com>
2015-10-17 11:53:14 +1300

    edit axe script

--- Assets/Scripts/Particles/Axe.cs
+++ Assets/Scripts/Particles/Axe.cs
@@ -6,6 +6,7 @@ public class Axe : MonoBehaviour {
     public GameObject axe;
     private float speed = 20;
     private Vector3 location;
+    //private int count = 0;
     // Use this for initialization
     void Start () {
         
@@ -13,8 +14,16 @@ public class Axe : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        location = owl.transform.position;
-        axe.transform.Rotate(-90f, 0.0f, 0.0f);
-        axe.transform.TransformDirection(location);
+        //if (count > 100)
+        //{
+            location = owl.transform.position;
+            GameObject instantiatedProjectile = Instantiate(axe, transform.position, transform.rotation) as GameObject;
+            instantiatedProjectile.transform.Rotate(-90f, 0.0f, 0.0f);
+        instantiatedProjectile.transform.position = Vector3.Lerp(instantiatedProjectile.transform.position, location, 0.05f);
+        //Destroy the arrow after 10 seconds
+        Destroy(instantiatedProjectile, 10);
+          //  count++;
+        //}
+        //count = 0;
     }
 }
