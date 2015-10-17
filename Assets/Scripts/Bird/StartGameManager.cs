0e17f93ca6e0109ebde079bf9bbe34aa01a48e33
hyub098 <hyub098@aucklanduni.ac.nz>
2015-10-17 18:22:09 +1300

    radar bug fix

--- Assets/Scripts/Bird/StartGameManager.cs
+++ Assets/Scripts/Bird/StartGameManager.cs
@@ -12,6 +12,8 @@ public class StartGameManager : MonoBehaviour {
 
 	public Canvas gameOver;
 	public Canvas mountainHelp;
+	public Camera radarCam;
+	private Radar radar;
 	private bool started;
 
 	// Use this for initialization
@@ -21,6 +23,7 @@ public class StartGameManager : MonoBehaviour {
 		collision = GetComponent< Collision> ();
 		rb = GetComponent<Rigidbody> ();
 		anim = GetComponent<Animation> ();
+		radar = radarCam.GetComponent<Radar> ();
 		started = false;
 		beforeStart ();
 
@@ -50,12 +53,14 @@ public class StartGameManager : MonoBehaviour {
 		collision.enabled = true;
 		mountainHelp.enabled = false;
 		rb.useGravity = true;
+		radar.enabled = true;
 		started = true;
 	}
 
 	void beforeStart(){
 		flymovement.enabled = false;
 		gameOver.enabled = false;
+		radar.enabled = false;
 
 //		mailcount.enabled = false;
 		collision.enabled = false;
