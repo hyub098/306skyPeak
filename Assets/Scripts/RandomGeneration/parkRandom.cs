d224e5725c4f23a4341952518ae3ad02ece60783
Chahat Chawla <ccha504@aucklanduni.ac.nz>
2015-10-17 15:30:22 +1300

    working on park random generation

--- Assets/Scripts/RandomGeneration/parkRandom.cs
+++ Assets/Scripts/RandomGeneration/parkRandom.cs
@@ -13,21 +13,21 @@ public class parkRandom : MonoBehaviour {
 		myList.Add(new Vector3(32,4 +UnityEngine.Random.value*15, 65 ));
 		myList.Add(new Vector3(50,4 +UnityEngine.Random.value*15, 52 ));
 		myList.Add(new Vector3(36,4 +UnityEngine.Random.value*15, 42 ));
-		myList.Add(new Vector3(43,4 +UnityEngine.Random.value*15, 10 ));
+		myList.Add(new Vector3(43,4 +UnityEngine.Random.value*15, 17 ));
 
 
 		myList.Add(new Vector3(75,6 +UnityEngine.Random.value*15, 15 ));
 		myList.Add(new Vector3(9,6 +UnityEngine.Random.value*15, 50 ));
-		myList.Add(new Vector3(31,10 +UnityEngine.Random.value*15, 22 ));
-		myList.Add(new Vector3(11,10 +UnityEngine.Random.value*15, 35 ));
-		myList.Add(new Vector3(75,15 +UnityEngine.Random.value*15, 68 ));
+		myList.Add(new Vector3(31,6 +UnityEngine.Random.value*15, 22 ));
+		myList.Add(new Vector3(11,6 +UnityEngine.Random.value*15, 35 ));
+		myList.Add(new Vector3(75,6 +UnityEngine.Random.value*15, 68 ));
 
-		myList.Add(new Vector3(53,10+UnityEngine.Random.value*15, 78 ));
-		myList.Add(new Vector3(75,15+UnityEngine.Random.value*15, 72 ));
-		myList.Add(new Vector3(33,15 +UnityEngine.Random.value*15, 15 ));
-		myList.Add(new Vector3(18,4 +UnityEngine.Random.value*15, 55 ));
-		myList.Add(new Vector3(23,15 +UnityEngine.Random.value*15, 67 ));
-		myList.Add(new Vector3(7,4 +UnityEngine.Random.value*15, 6 ));
+		myList.Add(new Vector3(53,6+UnityEngine.Random.value*15, 78 ));
+		myList.Add(new Vector3(75,6+UnityEngine.Random.value*15, 72 ));
+		myList.Add(new Vector3(33,6 +UnityEngine.Random.value*15, 15 ));
+		myList.Add(new Vector3(18,6 +UnityEngine.Random.value*15, 55 ));
+		myList.Add(new Vector3(23,6 +UnityEngine.Random.value*15, 67 ));
+		myList.Add(new Vector3(7,6 +UnityEngine.Random.value*15, 6 ));
 		
 		int number = Mathf.RoundToInt(UnityEngine.Random.value*15);
 		transform.position=myList[number];
