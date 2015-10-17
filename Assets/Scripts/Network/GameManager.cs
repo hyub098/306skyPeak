5abe8a39db58530453880279b72daa66ae40ffa5
Davidjin0418 <bjin718@aucklanduni.ac.nz>
2015-10-18 10:34:22 +1300

    add door prefab,update multiplayer manager

--- Assets/Scripts/Network/GameManager.cs
+++ Assets/Scripts/Network/GameManager.cs
@@ -3,8 +3,7 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
-	// this is a object name (must be in any Resources folder) of the prefab to spawn as player avatar.
-	// read the documentation for info how to spawn dynamically loaded game objects at runtime (not using Resources folders)
+	//prefab name, should be in Resource folder
 	public string playerPrefabName = "OnlineOwl";
 	public Camera camera;
 	void OnJoinedRoom()
@@ -14,8 +13,8 @@ public class GameManager : MonoBehaviour {
 	
 	IEnumerator OnLeftRoom()
 	{
-		//Easy way to reset the level: Otherwise we'd manually reset the camera
-		
+		//reset everything
+
 		//Wait untill Photon is properly disconnected (empty room, and connected back to main server)
 		while(PhotonNetwork.room!=null || PhotonNetwork.connected==false)
 			yield return 0;
@@ -32,7 +31,6 @@ public class GameManager : MonoBehaviour {
 		GameObject bird = PhotonNetwork.Instantiate(this.playerPrefabName, new Vector3(0.0f,0.0f,0.0f), Quaternion.identity, 0);
 		MoveMent controller = bird.GetComponent<MoveMent>();
 		// Enable the camera
-		
 		controller.isControllable = true;
 		Transform playerCam = bird.transform.Find ("Camera");
 		playerCam.gameObject.SetActive ( true);
